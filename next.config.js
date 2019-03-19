2bc8cdd97aeb4187836432a3fda3ca9cf8fdd65f
Aleksandar <aleksandar.gujas@gmail.com>
2019-03-19 18:44:00 +1000

    changed next.config to handle sass bundling

--- next.config.js
+++ next.config.js
@@ -1,5 +1,21 @@
-const withSass = require("@zeit/next-sass");
+const { PHASE_PRODUCTION_SERVER } =
+  process.env.NODE_ENV === 'development'
+    ? {} // We're never in "production server" phase when in development mode
+    : !process.env.NOW_REGION 
+      ? require('next/constants') // Get values from `next` package when building locally
+      : require('next-server/constants'); // Get values from `next-server` package when building on now v2
 
-module.exports = withSass( {
-  target: 'serverless'
-})
+module.exports = (phase, { defaultConfig }) => {
+  if (phase === PHASE_PRODUCTION_SERVER) {
+    // Config used to run in production.
+    return {};
+  }
+
+  const withSass = require('@zeit/next-sass');
+
+  return withSass({target:'serverless'});
+};
+
+// {
+//   	target: 'serverless'
+// 	}
