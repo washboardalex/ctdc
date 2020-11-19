4c0290eda2eee524d31927070fb1ccd22d5707f6
Aleksandar Gujas <aleksandar.gujas@gmail.com>
2020-11-19 20:50:13 +1000

    cancel that

--- pages/components/bookingcard/components/calendlywidget/CalendlyWidget.js
+++ pages/components/bookingcard/components/calendlywidget/CalendlyWidget.js
@@ -1,7 +1,7 @@
 import './CalendlyWidget.scss';
 
 const CalendlyWidget = () => (
-	<div class="calendly-inline-widget" data-url="https://calendly.com/cleveland-tooth/60min?hide_gdpr_banner=1" style={{overflow:'hidden'}}></div>
+	<div class="calendly-inline-widget" data-url="https://calendly.com/cleveland-tooth/60min?hide_gdpr_banner=1"></div>
 );
 
 export default CalendlyWidget;
