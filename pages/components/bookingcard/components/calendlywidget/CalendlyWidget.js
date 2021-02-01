cbfa36a2bcbc182ab373f089f8baa71c4bc51214
Aleksandar Gujas <aleksandar.gujas@gmail.com>
2021-02-01 22:06:42 +1000

    try again

--- pages/components/bookingcard/components/calendlywidget/CalendlyWidget.js
+++ pages/components/bookingcard/components/calendlywidget/CalendlyWidget.js
@@ -1,10 +1,7 @@
 import styles from './CalendlyWidget.module.scss';
 
 const CalendlyWidget = () => (
-<>
     <div class="calendly-inline-widget" data-url="https://calendly.com/cleveland-tooth?hide_landing_page_details=1&hide_gdpr_banner=1" style="min-width:320px;height:630px;"></div>
-    <script type="text/javascript" src="https://assets.calendly.com/assets/external/widget.js"></script>
-</>
 );
 
 export default CalendlyWidget;
