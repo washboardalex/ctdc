150910f9f6ada127108c46c9b5901a73443bfd0b
Aleksandar Gujas <aleksandar.gujas@gmail.com>
2021-02-01 21:57:25 +1000

    calendly fix

--- pages/components/bookingcard/components/calendlywidget/CalendlyWidget.js
+++ pages/components/bookingcard/components/calendlywidget/CalendlyWidget.js
@@ -1,7 +1,7 @@
 import styles from './CalendlyWidget.module.scss';
 
 const CalendlyWidget = () => (
-	<div class={`${styles.calendlyInlineWidget}`} data-url="https://calendly.com/cleveland-tooth/60min?hide_gdpr_banner=1"></div>
+	<div className={`${styles.calendlyInlineWidget}`} data-url="https://calendly.com/cleveland-tooth/60min?hide_gdpr_banner=1"></div>
 );
 
 export default CalendlyWidget;
